d10916e94f01e597755f09f81da07d834d154895
monishreeramesh <monishreemoni2000@gmail.com>
2024-03-03 13:13:24 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import About from './About';
 import AddBooks from './AddBooks';
 import './App.css';
@@ -9,60 +9,38 @@ import Nav from './Nav';
 import {Routes,Route, useNavigate} from 'react-router-dom'
 import Signin from './Signin';
 import Main from './Main';
-import api from './api/data'
 
 function App() {
 
-  useEffect(()=>{
-    const fetch=async()=>{
-      try{
-        const result=await api.get("/books")
-        setBooks(result.data)
-      }catch(err){
-          console.log(err.message)
-      }
-
-
-    }
-    fetch();
-  },[])
+  
   
   const [Books,setBooks]=useState([])
   const [searchbook,setsearchbooks]=useState('')
   const [returnbookName,setreturnbook]=useState('')
   const [returnbookdate,setbookdate]=useState('')
   const navigate=useNavigate('')
-  const handelSubmit=async(e)=>{
+  const handelSubmit=(e)=>{
     e.preventDefault()
     const id=Books.length ? Books[Books.length-1].id+1:1;
     const date=new Date().toLocaleString();
     const addBooks={id,title:returnbookName,date,returndate:returnbookdate}
-    try{
-      const respond=await api.post('/books',addBooks)
-      const books=[...Books,respond.data]
-      setBooks(books)
-      setreturnbook('')
-      setbookdate('')
-      navigate('/home')
-      localStorage.setItem("libraryapp",JSON.stringify(addBooks))
+    const books=[...Books,addBooks]
+    setBooks(books)
+    setreturnbook('')
+    setbookdate('')
+    navigate('/home')
+    localStorage.setItem("libraryapp",JSON.stringify(addBooks))
 
-    }catch(err){
-      console.log(err.message)
-    }
     
-
   }
-  const DeleteBook=async(id)=>{
-    try{
+  const DeleteBook=(id)=>{
+    
       const listBooks=Books.filter((book)=> book.id!==id)
-      await api.delete(`/books/${id}`,listBooks)   
+         
       setBooks(listBooks)
       localStorage.setItem("libraryapp",JSON.stringify(listBooks))
 
-    }catch(err){
-      console.log(err.message)
-    }
-
+   
   }
 
   return (
